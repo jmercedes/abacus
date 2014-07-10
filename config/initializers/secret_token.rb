b381ed7a4d0d1e27b726e458408e66e087fb6292
Juan Mercedes <j.mercedes@thedesignpill.com>
2014-07-10 15:37:21 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AbacusApp::Application.config.secret_key_base = '519ca7d9e22886f98d9722caeb7653b9660517d32a3dfc5109f03832ce5248db2c52ac1bdfe824e3f3b88de382e5c64cb4879a165485c28cda04bc0ff1c7a08a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AbacusApp::Application.config.session_store :cookie_store, key: '_abacus_app_session'
