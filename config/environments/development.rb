44e73d7b61dab3cd011d6762e1866c0c537345c2
Juan Mercedes <j.mercedes@thedesignpill.com>
2015-02-25 19:23:21 -0400

    fix bug on confirmation template

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,12 +31,13 @@ AbacusApp::Application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   
   ActionMailer::Base.smtp_settings = {
-    :address        => "smtp.sendgrid.net",
-    :port           => "25",
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => "heroku.com"
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
   
 end
