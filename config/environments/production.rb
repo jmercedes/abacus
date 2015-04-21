21e5d8df28a60915941f896159af10007c15a6e9
Juan Mercedes <j.mercedes@thedesignpill.com>
2015-04-21 18:22:06 -0400

    add assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,7 @@ AbacusApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.assets.precompile += %w( admin/dashboard.css )
   
   config.action_mailer.default_url_options = { :host => 'https://abacus-app.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
@@ -88,6 +89,5 @@ AbacusApp::Application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
-  config.assets.precompile += %w( dashboard.css.scss )
   
 end
