c793e85adb8969fbf034d2e81e5e1a216e0a4c89
Juan Mercedes <j.mercedes@thedesignpill.com>
2015-04-21 17:43:40 -0400

    trying to fix birth_date

--- db/migrate/20150407121834_fix_birth_date_columns.rb
+++ db/migrate/20150407121834_fix_birth_date_columns.rb
@@ -1,6 +1,6 @@
 class FixBirthDateColumns < ActiveRecord::Migration
   def change
-    change_column :guarantors, :birth_date, 'integer USING CAST(birth_date AS date)'
+    change_column :guarantors, :birth_date, 'date USING CAST(birth_date AS date)'
     change_column :profiles, :father_birthdate, :date
     change_column :profiles, :mother_birthdate, :date
     change_column :profiles, :spouse_birthdate, :date
