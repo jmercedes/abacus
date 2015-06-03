899521f4f827e1ffd85b3e7e675aed1521c0d49c
Juan Mercedes <j.mercedes@thedesignpill.com>
2015-06-03 17:12:25 -0400

    remove

--- db/migrate/20150523001351_add_late_fee_to_loans.rb
+++
@@ -1,6 +0,0 @@
-class AddLateFeeToLoans < ActiveRecord::Migration
-  def change
-    add_column :loans, :late_fee, :decimal, default: 0
-    add_column :loans, :capital_payment, :decimal, default: 0
-  end
-end
